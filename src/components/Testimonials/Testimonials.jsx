401f6bd3bf252676c593ce99414ccf82084876df
cosisaxis <kishanelewis2@gmail.com>
2022-09-13 18:50:53 -0500

    testimonial-animation-done

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,7 +3,9 @@ import "./Testimonials.css"
 import { testimonialsData } from "../../data/testimonialsData"
 import leftArrow from "../../assets/leftArrow.png"
 import rightArrow from "../../assets/rightArrow.png"
+import {motion} from "framer-motion";
 const Testimonials = () => {
+    const transition = {type: 'spring', duration: 3};
     const [selector, setSelector] = useState(0);
     const testimonialsLength = testimonialsData.length;
 
@@ -14,9 +16,15 @@ const Testimonials = () => {
     <span>Testimonials</span>
     <span className='transparent-text'>what they</span>
     <span>say about us</span>
-    <span>
+    <motion.span
+    key={selector}
+    initial={{opacity:0, x:-100}}
+    animate={{opacity: 1, x:0}}
+    exit={{opacity:0, x:100}}
+    transition={transition}
+    >
         {testimonialsData[selector].review}
-    </span>
+    </motion.span>
     <span>
         <span style={{color: 'var(--blue)'}}>
             {testimonialsData[selector].name}
@@ -25,9 +33,23 @@ const Testimonials = () => {
     </span>
    </div>
    <div className="right-testimonials">
-    <div></div>
-    <div></div>
-    <img src={testimonialsData[selector].image} alt="" />
+    <motion.div
+    initial={{ opacity: 0, x: -100 }}
+    transition={{ ...transition, duration: 2 }}
+    whileInView={{ opacity: 1, x:0} }
+    ></motion.div>
+    <motion.div
+    initial={{ opacity: 0, x: 100 }}
+    transition={{ ...transition, duration: 2 }}
+    whileInView={{opacity: 1, x:0}}
+    ></motion.div>
+    <motion.img
+    key={selector}
+    initial={{opacity:0, x:100}}
+    animate={{opacity: 1, x:0}}
+    exit={{opacity:0, x:-100}}
+    transition={transition}
+     src={testimonialsData[selector].image} alt="" />
     <div className="arrows">
         <img
         onClick={() => {
